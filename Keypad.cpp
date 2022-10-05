ecd95a785c3532842898128b37cfa189637c3a5c
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2022-10-05 15:13:18 +0200

    Add files via upload

---
+++ Keypad.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Ismail Magdy on 05/10/2022.
+//
+
+#include "Keypad.h"
+using namespace std;
+
+std::string Keypad::getInput() const {
+    string input;
+    cin>>input;
+    return input;
+}
---
+++ Keypad.h
@@ -0,0 +1,15 @@
+//
+// Created by Ismail Magdy on 05/10/2022.
+//
+
+#ifndef MAIN_CPP_KEYPAD_H
+#define MAIN_CPP_KEYPAD_H
+#include <iostream>
+
+class Keypad {
+public:
+    std::string getInput()const;
+};
+
+
+#endif //MAIN_CPP_KEYPAD_H
