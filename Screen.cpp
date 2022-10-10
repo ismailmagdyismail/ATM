94fb17596614782f561a2aec72c5f2ea39f1a5a6
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2022-10-10 13:58:33 +0200

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -1,12 +1,9 @@
-//
-// Created by Ismail Magdy on 04/10/2022.
-//
 
 #include "Screen.h"
 using namespace std;
 
 void Screen::display(const std::string &message) const {
-    cout<<message<<'\n';
+    cout<<message;
 }
 
 
