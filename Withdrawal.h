d8f1a96100053c27995f7f70ee7ca03fbb29ac76
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2022-10-10 14:00:18 +0200

    Update Withdrawal.h

--- Withdrawal.h
+++ Withdrawal.h
@@ -1,6 +1,4 @@
-//
-// Created by Ismail Magdy on 08/10/2022.
-//
+
 
 #ifndef MAIN_CPP_WITHDRAWAL_H
 #define MAIN_CPP_WITHDRAWAL_H
@@ -9,19 +7,20 @@
 #include "CashDispenser.h"
 #include "Keypad.h"
 #include "DataBase.h"
-class Withdrawal :Transaction{
+class Withdrawal :public Transaction{
 public:
 
-    Withdrawal(Account* account,Screen* screen,Keypad* keypad,CashDispenser* cashDispenser);
+    Withdrawal(Account* account,Screen* screen,Keypad* keypad,CashDispenser* cashDispenser,DataBase* dataBase);
+    virtual ~Withdrawal()=default;
     virtual void performTransactions() override;
 
 private:
-    int Menu[5];
-    double amount;
-    CashDispenser* dispenser;
-    Keypad* keypad;
-    void displayMenu()const;
-    bool validInput(int input)const;
+    double amount;// amount of money to be withdrawn
+    CashDispenser* dispenser;// interact with ATM's cash dispenser
+    Keypad* keypad;// interact with ATM's keypad
+    DataBase* dataBase;
+    int displayMenu()const;// display list of available transactions
+    bool validInput(int input)const;// check if input is valid
 };
 
 
