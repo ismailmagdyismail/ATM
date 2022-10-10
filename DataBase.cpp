c257ab94c62a78234b5d3467fff328b1f8f194fb
Ismail Magdy <92678464+ismailmagdyismail@users.noreply.github.com>
2022-10-10 13:57:21 +0200

    Update DataBase.cpp

--- DataBase.cpp
+++ DataBase.cpp
@@ -1,14 +1,24 @@
-//
-// Created by Ismail Magdy on 05/10/2022.
-//
 
 #include "DataBase.h"
 #include <map>
 #include "Account.h"
 using namespace std;
-std::pair<bool, Account> DataBase::authenticateAccount(const string &accountNumber, const string &PIN)const{
 
-    Account dummy ("","",0);  // DUMMY ACCOUNT in case of not finding account
+DataBase::DataBase() {
+
+    // two accounts for  testing
+    Account account1(12345, 12345, 1200.0);
+    Account account2(98765, 56789, 200.0);
+
+    // store the accounts in database
+    accounts.push_back(account1);
+    accounts.push_back(account2);
+}
+
+
+std::pair<bool, Account> DataBase::authenticateAccount(const int &accountNumber, const int &PIN)const{
+
+    Account dummy (0,0,0);  // DUMMY ACCOUNT in case of not finding account
     pair<bool,Account> searchResult={false,dummy}; // result of search {found or not,account searched}
 
     int idx = findAccount(accountNumber); // searching for an account with this account number
@@ -30,7 +40,7 @@ void DataBase::updateAccount(const Account & account) {
 }
 
 
-int DataBase::findAccount(const string &accountNumber) const { // searching for an account with this account number
+int DataBase::findAccount(const int &accountNumber) const { // searching for an account with this account number
     for(int i = 0 ;i<accounts.size();i++)
     {
         if(accounts[i].getAccountNumber()==accountNumber)
